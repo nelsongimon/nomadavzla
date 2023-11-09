91cb6707b3e38b3b86030a9414d195ed8e456cfe
Nelson Gimon <nelson.gimon@gmail.com>
2023-11-09 18:18:26 -0400

    Api folder deleted

--- app/api/products/search/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const query = searchParams.get("query");
-
-  return NextResponse.json(query);
-
-}
