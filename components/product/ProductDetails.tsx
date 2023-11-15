1d4d3ee13113f852f54fc0b0bf17fff52fd21f06
Nelson Gimon <nelson.gimon@gmail.com>
2023-11-15 12:14:02 -0400

    Product details attributes values order

--- components/product/ProductDetails.tsx
+++ components/product/ProductDetails.tsx
@@ -12,6 +12,17 @@ interface ProductDetailsProps {
 export default function ProductDetails({
   product
 }: ProductDetailsProps) {
+  let attributes: { [key: string]: string | null } = {};
+  product.attributeValues.forEach((item) => {
+    if (item.attribute.name in attributes) {
+      attributes[item.attribute.name] = attributes[item.attribute.name] + ", " + item.name;
+    } else {
+      attributes = {
+        ...attributes,
+        [item.attribute.name]: item.name
+      }
+    }
+  });
   return (
     <Container>
       <div className="relative flex justify-center w-full items-center">
@@ -26,11 +37,11 @@ export default function ProductDetails({
         <div className="col-span-4">
           <div className="flex flex-col">
             <ul className="mt-5 flex flex-col gap-y-1">
-              {product.attributeValues.length > 0 && (
-                product.attributeValues.map((item) => (
-                  <li key={item.id} className="flex gap-x-3 bg-gray-50 py-2 px-3">
-                    <span className="text-lg font-semibold">{item.attribute.name}: </span>
-                    <span className="text-lg font-light">{item.name}</span>
+              {Object.entries(attributes).length > 0 && (
+                Object.entries(attributes).map(([key, value], index) => (
+                  <li key={index} className="flex gap-x-3 bg-gray-50 py-2 px-3">
+                    <span className="text-lg font-semibold">{key}: </span>
+                    <span className="text-lg font-light">{value}</span>
                   </li>
                 ))
               )}
@@ -38,14 +49,11 @@ export default function ProductDetails({
           </div>
         </div>
         <div className="col-span-8 p-0 lg:p-5">
-          <div className="aspect-video relative">
+          <div className="h-[300px] relative">
             <Image
               fill
-              src={product.specificationImage ? addAbsolutePathImage(product.specificationImage) : ""}
+              src={product.specificationImage ? addAbsolutePathImage(product.specificationImage) : "https://placehold.co/1000x300.png"}
               alt="Product Detail"
-              className="
-                object-contain
-              "
             />
           </div>
         </div>
