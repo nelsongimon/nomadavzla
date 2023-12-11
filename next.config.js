e2b216cc56880329d023209ac43ff8ba5625b2ca
Nelson Gimon <nelson.gimon@gmail.com>
2023-12-11 14:06:23 -0400

    Next config file updated

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/finalizar-compra",
+        headers: [
+         {
+           key: "Content-Security-Policy",
+           value: "upgrade-insecure-requests"
+         }
+        ],
+      },
+    ]
+  },
   images: {
     domains: [
       "www.nomadavzla.com",
@@ -7,7 +20,7 @@ const nextConfig = {
       "nomada-admin.test",
       "nomadavzla.store",
     ]
-  }
+  },
 }
 
 module.exports = nextConfig
