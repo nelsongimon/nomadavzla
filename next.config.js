f5bad9b05b3b1b54d8162a294550da668cd48baa
Nelson Gimon <nelson.gimon@gmail.com>
2023-12-11 15:14:34 -0400

    nextConfig changed

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/finalizar-compra",
-        headers: [
-         {
-           key: "Content-Security-Policy",
-           value: "upgrade-insecure-requests"
-         }
-        ],
-      },
-    ]
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/finalizar-compra",
+  //       headers: [
+  //        {
+  //          key: "Content-Security-Policy",
+  //          value: "upgrade-insecure-requests"
+  //        }
+  //       ],
+  //     },
+  //   ]
+  // },
   images: {
     domains: [
       "www.nomadavzla.com",
